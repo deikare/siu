be4b719be15efea5d3f6c5b25b39a32970924a11
XYZ <deikare@gmail.com>
2022-05-12 15:54:37 +0200

    Add container scripts

---
+++ run-script-in-container.sh
@@ -0,0 +1,22 @@
+CONTAINER="siu"
+FILE=""
+CONTAINER_DIRECTORY="/root"
+
+while getopts "c:f:d:" option; do
+  case $option in
+    c ) CONTAINER=$OPTARG
+    ;;
+    f ) FILE=$OPTARG
+    ;;
+    d ) CONTAINER_DIRECTORY=$OPTARG
+    ;;
+  esac
+done
+
+if [[ -z "$FILE" || -z "$CONTAINER" || -z "$CONTAINER_DIRECTORY" ]]; then #if no file specified
+    yell() { echo "$0: $*" >&2; }
+    die() { yell "$*"; exit 111; }
+    try() { "$@" || die "cannot $*"; }
+fi
+
+docker exec $CONTAINER bash -c "source /root/siu_ws/devel/setup.bash && python3 $CONTAINER_DIRECTORY/$FILE"
